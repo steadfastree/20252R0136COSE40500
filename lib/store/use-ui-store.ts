b741a061ed32429d68b7961ba9bf1687129a271c
Jeonghwan Park <pjhisgod@naver.com>
2026-01-23 03:49:03 +0900

    feat: add initial Zustand store for UI state

---
+++ lib/store/use-ui-store.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+interface UIState {
+  sidebarOpen: boolean;
+  toggleSidebar: () => void;
+  setSidebarOpen: (open: boolean) => void;
+}
+
+export const useUIStore = create<UIState>()(
+  devtools(
+    (set) => ({
+      sidebarOpen: true,
+      toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
+      setSidebarOpen: (open) => set({ sidebarOpen: open }),
+    }),
+    { name: 'UIStore' }
+  )
+);
